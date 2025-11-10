d531c7e2380ec912a9b9f906eea169fe0aa8c0c5
shamila37 <128484072+shamila37@users.noreply.github.com>
2025-11-10 08:47:47 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,6 @@
+// Create web server
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!\n');
+});
